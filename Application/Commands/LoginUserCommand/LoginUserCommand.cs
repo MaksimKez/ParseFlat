fa862d3bde85ef44b569cac29185e5189a4f1309
MaksimKez <prostomaxim765@gmail.com>
2025-08-01 14:41:50 +0200

    Transaction via behavior

--- Application/Commands/LoginUserCommand/LoginUserCommand.cs
+++ Application/Commands/LoginUserCommand/LoginUserCommand.cs
@@ -1,7 +1,8 @@
+using Application.Abstractions.Messaging;
 using Application.Dtos.Users;
 using Application.Responses;
 using MediatR;
 
 namespace Application.Commands.LoginUserCommand;
 
-public record class LoginUserCommand(LoginUserRequest Dto) : IRequest<LoginUserResult>;
+public record LoginUserCommand(LoginUserRequest Request) : ITransactionalCommand<LoginUserResult>;
