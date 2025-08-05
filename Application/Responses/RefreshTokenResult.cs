70c8e1d7ae1296b17163a32fa2328d5d60812418
MaksimKez <prostomaxim765@gmail.com>
2025-08-05 13:14:34 +0200

    token in RefreshTokenResult.cs is nullable

--- Application/Responses/RefreshTokenResult.cs
+++ Application/Responses/RefreshTokenResult.cs
@@ -4,7 +4,7 @@ public class RefreshTokenResult : IResult
 {
     public bool IsSuccess { get; init; }
     public string? ErrorMessage { get; init; }
-    public string Token { get; init; }
+    public string? Token { get; init; }
     
     public static RefreshTokenResult Success(string token) => new RefreshTokenResult { IsSuccess = true, Token = token };
     public static RefreshTokenResult Failure(string errorMessage) => new RefreshTokenResult { IsSuccess = false, ErrorMessage = errorMessage };
