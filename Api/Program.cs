6ed823807eed16b001ce06269e65cd2589fa65b1
MaksimKez <prostomaxim765@gmail.com>
2025-10-29 15:04:44 +0100

    use swagger anyway

--- Api/Program.cs
+++ Api/Program.cs
@@ -105,11 +105,8 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowFrontend");
 
